af9d16925468c5b695b4504f748eb265bbeedbff
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-02-20 16:45:03 +0300

    added model classes

---
+++ src/app/model/user/IUser.ts
@@ -0,0 +1,7 @@
+export interface IUser {
+  id: number
+  email: string
+  phoneNumber: string
+  firstName: string
+  lastName: string
+}
---
+++ src/app/model/user/User.ts
@@ -0,0 +1,26 @@
+import {IUser} from "./IUser";
+
+export class User implements IUser {
+  id: number
+  email: string
+  phoneNumber: string = ''
+  firstName: string = ''
+  lastName: string = ''
+
+  constructor(userInfo?: IUser) {
+    if (userInfo) {
+      this.id = userInfo.id
+      this.email = userInfo.email
+      this.phoneNumber = userInfo.phoneNumber ? userInfo.phoneNumber : ''
+      this.firstName = userInfo.firstName ? userInfo.firstName : ''
+      this.lastName = userInfo.lastName ? userInfo.lastName : ''
+    }
+  }
+
+  getFullNameOrEmail(): string {
+    if (!this.firstName && !this.lastName) {
+      return this.email
+    }
+    return `${this.firstName} ${this.lastName}`.trim()
+  }
+}
