205be522bcc595c10e60b7643fc54247f435087b
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-02-20 16:53:58 +0300

    implemented auth guard

--- src/app/guard/auth.guard.ts
+++ src/app/guard/auth.guard.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable} from 'rxjs';
+import {AuthenticationService} from "../service/authentication.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+  constructor(private authService: AuthenticationService, private router: Router) {
   }
-  
+
+  canActivate(route: ActivatedRouteSnapshot,
+              state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (!this.authService.isAuthenticated()) {
+      this.router.navigate(['questionnaires'])
+      return false
+    }
+    return true
+  }
+
+
 }
