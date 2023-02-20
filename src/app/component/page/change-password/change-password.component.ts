6aa11b5f59e0103eb6e8cecf5a7a5604d8c41985
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-02-20 17:10:53 +0300

    refactored change password component

--- src/app/component/page/change-password/change-password.component.ts
+++ src/app/component/page/change-password/change-password.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {passwordConfirmationValidator} from "../../../validator/password.confirmation.validator";
 
 @Component({
   selector: 'app-change-password',
@@ -6,5 +8,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent {
+  changePasswordForm: FormGroup = this.formBuilder.group({
+    currentPassword: ['', Validators.required],
+    newPassword: ['', Validators.required],
+    confirmNewPassword: ['', Validators.required]
+  }, {validators: passwordConfirmationValidator('newPassword', 'confirmNewPassword')})
 
+  constructor(private formBuilder: FormBuilder) {
+
+  }
+
+  submit(): void { // todo implement
+    if (this.changePasswordForm.invalid) {
+      alert("The form is filled with invalid data")
+      return
+    }
+  }
+
+  get currentPassword() {
+    return this.changePasswordForm.controls['currentPassword']
+  }
+
+  get newPassword() {
+    return this.changePasswordForm.controls['newPassword']
+  }
+
+  get confirmNewPassword() {
+    return this.changePasswordForm.controls['confirmNewPassword']
+  }
 }
