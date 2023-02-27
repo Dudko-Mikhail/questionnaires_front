caa8181f2e90264321c96615666bfd5a6103f274
Dudko Mikhail <dudkomikhail01@gmail.com>
2023-02-27 15:34:05 +0300

    added new environments

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  apiUrl: "http://localhost:8080"
+  apiUrl: 'http://localhost:8080',
+  notificationLiveTime: 5000,
+  resetPasswordVerificationCodeStorageKey: 'rpvck',
+  resetPasswordEmailStorageKey: 'rpek',
+  continueRegistrationEmailStorageKey: 'crek'
 };
